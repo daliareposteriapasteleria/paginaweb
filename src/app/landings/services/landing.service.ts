77474128a0ead40da7073bf4e977b40db8f142fc
edissonchamorroc <john.chamorro@udea.edu.co>
2023-10-04 12:10:35 -0500

    se agrega responsive

--- src/app/landings/services/landing.service.ts
+++ src/app/landings/services/landing.service.ts
@@ -10,9 +10,9 @@ export class landingService {
             title: 'TORTAS',
             description: '¿Imaginas una torta tan única como tú? En nuestra reposteria, transformamos tus sueños en delicias hechas a medida. Desde bodas hasta cumpleaños, cada torta es una obra de arte personalizada, diseñada exclusivamente para ti. ¡Déjanos transformar tu ocasión especial en un momento inolvidable! Haz clic en "Personalizar Ahora" y comienza el viaje hacia la torta perfecta para ti.',
             urls: [
-                '../../../../assets/imgs/cakes/flowers-cake.png',
-                '../../../../assets/imgs/cakes/paw-patrol-cake.png',
-                '../../../../assets/imgs/cakes/purple-cake.png',
+                './assets/imgs/cakes/flowers-cake.png',
+                './assets/imgs/cakes/paw-patrol-cake.png',
+                './assets/imgs/cakes/purple-cake.png',
             ]
         }
 
